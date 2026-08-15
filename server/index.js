14f9b919b45bfe6e394cee5105d8b08eb4db677c
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-15 08:54:50 +0300

    Instantiating express server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running")
+})
