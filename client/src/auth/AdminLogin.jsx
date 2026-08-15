9f4d4efb43680857b50bd53c5e2d6e2987996369
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-15 16:31:56 +0300

    Adding inputs to login page

--- client/src/auth/AdminLogin.jsx
+++ client/src/auth/AdminLogin.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import Input from '../components/Input'
+import { Lock, Mail } from 'lucide-react'
 
 function AdminLogin() {
   return (
     <div className='flex justify-center items-center h-screen px-2'>
-        <div className='max-w-md w-full rounded-xl shadow-xl p-2 bg-white/50 backdrop-blur-2xl'>
+        <form className='max-w-md w-full rounded-xl shadow-xl p-2 bg-white/50 backdrop-blur-2xl'>
         <h1 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-blue-400 to-blue-600
             text-transparent bg-clip-text'>Login Admin</h1>
-        </div>
+              <Input
+                icon={Mail}
+                type="email"
+                name="email"
+                placeholder="Email"
+            />
+             <Input
+                icon={Lock}
+                type="password"
+                name="password"
+                placeholder="Password"
+            />
+            <button className='w-full text-white bg-gradient-to-r from-blue-400 to-blue-600 py-2
+            font-extrabold cursor-pointer rounded-md text-xl
+            '>Login</button>
+        </form>      
     </div>
   )
 }
--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -4,12 +4,12 @@ function Input({icon:Icon,...props}) {
   return (
     <div className='relative mb-6'>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-            <Icon className="h-6 text-blue-950"/>
+            <Icon className="h-6 text-blue-500"/>
         </div>
         <input
         {...props}
-            className='w-full pl-10 pr-3 py-2 bg-gray-800/50 rounded-lg border border-blue-700
-            focus:border-lue-600 focus:ring-blue-500 text-white placeholder-blue-400 transition
+            className='w-full pl-10 pr-3 py-2 bg-white-800/50 rounded-lg border border-blue-700
+            focus:border-lue-600 focus:ring-blue-500 text-gray-900 placeholder-gray-400 transition
             duration-200'
         />
     </div>
