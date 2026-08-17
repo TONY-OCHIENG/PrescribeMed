9ef256dc11c05af4f53efd490da54338f48de6ec
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 17:41:29 +0300

    Adding appointment's table

--- client/src/pages/Appointments.jsx
+++ client/src/pages/Appointments.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Appointments() {
   return (
-    <div>Appointments</div>
+    <div className='w-full md:max-w-[90%] px-2 mx-auto '>
+      <h1 className='mt-25 text-gray-600 font-extrabold'>Appointment history</h1>
+       <div className='p-4 rounded-md shadow-md mt-4 bg-white h-[550px] overflow-y-auto overflow-x-auto'>
+        <table className='w-full'>
+            <thead className='text-xs text-left'>
+                <th>Doctor's Image</th>
+                <th>Doctor's Name</th>
+                <th>Patient's Image</th>
+                <th>Patient's Name</th>
+                <th>Appointment Date</th>
+                <th>Appointment Fee</th>
+                <th>Status</th>
+                <th>Action</th>
+            </thead>
+        </table>
+      </div> 
+    </div>
   )
 }
 
