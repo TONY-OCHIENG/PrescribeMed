fa398b3aa44b2b313b4bad42e5fe22ac5eec04d5
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 16:53:39 +0300

    Adding recent appointments table

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div className='w-full md:max-w-[80%] px-2 mx-auto '>
+    <div className='w-full md:max-w-[90%] px-2 mx-auto '>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-25'>
         <div className='p-4 bg-white rounded-md shadow-md flex items-center gap-5'>
             <BriefcaseMedicalIcon className='h-10 w-10 text-blue-500'/>
@@ -47,7 +47,21 @@ function Dashboard() {
                 <p>Canceled</p>
             </div>
         </div>
-    </div>
+      </div>
+      <h1 className='text-gray-600 mt-10 font-extrabold'>Recent Appointments</h1>
+      <div className='p-4 rounded-md shadow-md mt-5 bg-white h-[320px] overflow-y-auto overflow-x-auto'>
+        <table className='w-full'>
+            <thead className='text-xs text-left'>
+                <th>Doctor's Image</th>
+                <th>Doctor's Name</th>
+                <th>Patient's Image</th>
+                <th>Patient's Name</th>
+                <th>Appointment Date</th>
+                <th>Appointment Fee</th>
+                <th>Status</th>
+            </thead>
+        </table>
+      </div>      
     </div>    
   )
 }
