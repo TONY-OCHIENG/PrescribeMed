1142f9308cf0e7e6e93db0ff0ab5c8d7579b0a73
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 13:02:28 +0300

    Adding endpoint that handles logout

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { adminLogin, authAdmin } from '../controllers/auth.controller.js'
+import { adminLogin, authAdmin, logoutAdmin } from '../controllers/auth.controller.js'
 import { auth } from '../middlewares/auth.js'
 
 const authRoutes = express.Router()
 authRoutes.post("/login",adminLogin)
 authRoutes.get("/authAdmin",auth,authAdmin)
+authRoutes.get("/logoutAdmin",logoutAdmin)
 export default authRoutes
