fbc020f1a251126f762df61cbe119e466d062748
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 11:07:27 +0300

    Authenticating admin on the client side

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth"
 function Admin() {
+    const [adminID,setAdminID] = useState(null)
+    const navigate = useNavigate()
+    axios.defaults.withCredentials = true
+    useEffect(() => {
+        axios.get(`${API_URL}/authAdmin`)
+        .then((response) => {
+            if (response.data.success) {
+                setAdminID(response.data.adminID)
+            } else {
+                navigate('/login')
+            }
+        })
+    },[adminID])
   return (
     <div>Admin</div>
   )
