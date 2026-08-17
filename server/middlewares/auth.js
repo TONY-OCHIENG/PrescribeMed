76a4040295f4c4e9fca801cc2b79241fb72c1333
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 11:24:19 +0300

    Refactoring auth middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,11 +5,11 @@ dotenv.config()
 export const auth = (request,response,next) => {
     const token = request.cookies.token
     if (!token) {
-        return response.status(403).json({success: false, message:"Unauthorised access"})
+        return response.status(200).json({success: false, message:"Unauthorised access"})
     } else {
         jwt.verify(token,process.env.SECRET,(err,decoded) => {
             if (err) {
-                return response.status(401).json({status:false, message:"Access denied"})
+                return response.status(200).json({success:false, message:"Access denied"})
             } else {
                 request.adminID = decoded.adminID
                 next()
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { adminLogin, authAdmin } from '../controllers/auth.controller.js'
+import { auth } from '../middlewares/auth.js'
 
 const authRoutes = express.Router()
 authRoutes.post("/login",adminLogin)
-authRoutes.get("/adminAuth",authAdmin)
+authRoutes.get("/authAdmin",auth,authAdmin)
 export default authRoutes
