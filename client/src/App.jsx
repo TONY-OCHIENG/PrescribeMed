8a29664063304c384920d550fdc7ef8f70a9e3c8
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 12:43:59 +0300

    Adding routes on admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import AdminLogin from './auth/AdminLogin'
 import Admin from './pages/Admin'
+import Dashboard from './pages/Dashboard'
+import Appointments from './pages/Appointments'
+import Doctors from './pages/Doctors'
+import AddDoctors from './pages/AddDoctors'
 
 function App() {
   return (
     <div className='bg-gray-100 h-[100vh]'>
       <Routes>
         <Route path='/login' element={<AdminLogin/>}/>
-        <Route path='/admin' element={<Admin/>}></Route>
+        <Route path='/admin' element={<Admin/>}>
+         <Route path='' element={<Dashboard/>}/>
+         <Route path='/admin/appointments' element={<Appointments/>}/>
+         <Route path='/admin/doctors' element={<Doctors/>}/>
+         <Route path='/admin/add-doctors' element={<AddDoctors/>}/>
+        </Route>
       </Routes>
     </div>
   )
