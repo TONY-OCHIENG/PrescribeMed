7b8ce908d3291af0713357c9b58f58dfc6101744
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-17 09:35:54 +0300

    Configuring cors to accept request from client

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:5173',
+    methods:['PUT','GET','POST','DELETE'],
+    credentials:true,
+})) 
 app.use(cookieParser())
 app.use("/api/auth/",authRoutes)
 app.listen(process.env.PORT, () => {
