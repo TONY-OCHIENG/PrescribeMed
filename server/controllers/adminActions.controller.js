25b9857649475bf9794838337eb8811f69d5b752
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-18 15:08:52 +0300

    Hashing doctor's password

--- server/controllers/adminActions.controller.js
+++ server/controllers/adminActions.controller.js
@@ -1,3 +1,4 @@
+import { hashPassword } from "../configs/hashPassword.js"
 
 export const addDoctors = (request,response) => {
     const {firstName,lastName, phone, email,password,speciality,experience,
@@ -13,4 +14,8 @@ export const addDoctors = (request,response) => {
     ) {
         return response.status(200).json({success: false, message: "Please fill all fields"})
     }
+
+    const hpassword = hashPassword(password)
+
+    
 }
