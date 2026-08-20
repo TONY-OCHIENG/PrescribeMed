a630b4a564c655cf8d30c00c4bfff261ded02424
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-20 15:58:49 +0300

    Adding closing add apening of dialogue functionality

--- client/src/pages/Doctors.jsx
+++ client/src/pages/Doctors.jsx
@@ -32,6 +32,7 @@ function Doctors() {
     axios.get(`http://localhost:5000/api/actions/getSingleDoctor/${id}`)
     .then((response) => {
       if (response.data.success) {
+        setOpen(!open)
         setSingleDoctor(response.data.result)
       } else {
         toast.error(response.data.message)
@@ -44,9 +45,9 @@ function Doctors() {
   return (
     <div className='relative'>
       <div className='relative mt-20 max-w-[90%] mx-auto screen flex justify-center items-center'>
-      <div className='absolute top-20 z-10 md:h-[500px] w-full md:w-[50%] bg-white rounded-xl shadow-md p-4'>
+      <div className={`absolute top-20 z-10 md:h-[500px] w-full md:w-[50%] bg-white rounded-xl shadow-md p-4 ${open ? "block" : "hidden"}`}>
         <h1 className='text-center font-extrabold'>Doctor's Details</h1>
-        <X className='absolute top-5 right-5 text-blue-600 cursor-pointer'/>
+        <X onClick={() => setOpen(!open)} className='absolute top-5 right-5 text-blue-600 cursor-pointer'/>
         <div>
           {
             singleDoctor.map((item) => (
@@ -87,7 +88,7 @@ function Doctors() {
         ))} 
       </div>    
      </div>
-      <div className='absolute top-0 w-full h-[660px] bg-black/50 hidden'></div>  
+      <div className={`${open ? "block" : "hidden"} absolute top-0 w-full h-[660px] bg-black/50`}></div>  
     </div>    
   )
 }
