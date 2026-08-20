be39aa2310ab9c20828083344345519ec979697a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-20 14:58:03 +0300

    Adding a div with a 50 opacity

--- client/src/pages/Doctors.jsx
+++ client/src/pages/Doctors.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Loader } from 'lucide-react'
+import { Loader, X } from 'lucide-react'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
@@ -28,12 +28,18 @@ function Doctors() {
   },[])
 
   return (
-    <div className='relative mt-20 max-w-[90%] mx-auto screen flex justify-center items-center'>
-      <div className='absolute top-20 h-[500px] w-full md:w-[50%] bg-white rounded-xl shadow-md'></div>
+    <div className='relative'>
+      <div className='relative mt-20 max-w-[90%] mx-auto screen flex justify-center items-center'>
+      <div className='absolute top-20 z-10 h-[500px] w-full md:w-[50%] bg-white rounded-xl shadow-md p-4'>
+        <h1 className='text-center font-extrabold'>Doctor's Details</h1>
+        <X className='absolute top-5 right-5 text-blue-600 cursor-pointer'/>
+      </div>
       <div className='grid grid-cols-2 md:grid-cols-5 gap-5'>
         {loading ? <LoaderPage/> : doctor.map((item) => <DoctorsCard item={item}/>)}
-      </div>      
-    </div>
+      </div>    
+     </div>
+      <div className='absolute top-0 w-full h-[660px] bg-black/50 hidden'></div>  
+    </div>    
   )
 }
 
