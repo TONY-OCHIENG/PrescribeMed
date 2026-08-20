f28092606077a6099c7ed533715a5b83085700d7
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-20 08:47:48 +0300

    Adding loading animation after submiting doctor's details

--- client/src/pages/AddDoctors.jsx
+++ client/src/pages/AddDoctors.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Input from '../components/Input'
-import { Calendar, Camera, DollarSignIcon, Lock, Mail, Phone, Stethoscope, User } from 'lucide-react'
+import { Calendar, Camera, DollarSignIcon, Loader, Lock, Mail, Phone, Stethoscope, User } from 'lucide-react'
 import { useState } from 'react'
 import axios from 'axios'
 import toast from 'react-hot-toast'
@@ -18,7 +18,7 @@ function AddDoctors() {
     about:'',
     image:''
   })
-
+  const [loading,setLoading] = useState(false)
   const handleValueChange = (event) => {
     const { name, value} = event.target
     setValues((prev) => ({
@@ -27,6 +27,7 @@ function AddDoctors() {
     }))
   }
   const handleSubmit = (event) => {
+    setLoading(true)
     event.preventDefault()
     const formData = new FormData()
     formData.append('firstName',value.firstName)
@@ -40,27 +41,31 @@ function AddDoctors() {
     formData.append('about',value.about)
     formData.append('image',value.image)
 
-    axios.post('http://localhost:5000/api/actions/addDoctor',formData)
+    axios.post('http://localhost:5000/api/actions/addDoctor',formData) 
     .then((response) => {
       if (response.data.success) {
         toast.success(response.data.message)
       } else {
         toast.error(response.data.message)
+        setLoading(false)
       }
     })
     .catch((error) => {
       console.log(error)
+      toast.error("Something went wrong try again later")
+      setLoading(false)
     })
   }
   return (
     <div className='w-full md:max-w-[90%] px-2 mx-auto flex items-center justify-center'>
       <div className='bg-white rounded-md shadow-md p-4 mt-25 w-full md:w-[60%] flex justify-center items-center'>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div className='flex gap-10 w-full'>
              <Input
               icon={User}
               type="text"
               name="firstName"
+              id="firstName"
               onChange={handleValueChange}
               placeholder="First name"
             />     
@@ -125,12 +130,15 @@ function AddDoctors() {
               icon={Camera}
               type="file"
               name="image"
-              onChange={(event) => {setValues({...value, image: event.target.files[0]})}} 
-              placeholder="Image"
+              onChange={(event) => {setValues({...value, image: event.target.files[0]})}}
             />        
           </div>
           <textarea name="about" onChange={handleValueChange} placeholder="Doctor's description..." id="" className='p-2 border-blue-500 border outline:border-blue-500 w-full rounded-md h-[100px]'></textarea> 
-          <button className='py-2 px-8 bg-blue-500 text-white rounded-md font-extrabold mt-4 cursor-pointer'>Add Doctor</button>          
+          <button disabled={loading} className='py-2 px-8 bg-blue-500 text-white rounded-md font-extrabold mt-4 cursor-pointer disabled:opacity-80' type='submit'>
+            {
+              loading ? <Loader className='flex justify-center items-center animate-spin'/> : "Add Doctor"
+            }
+          </button>          
         </form>
       </div>
     </div>
