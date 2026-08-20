b0a9ead0a5debcbf9967acf18c460fa7defde082
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-20 12:31:32 +0300

    Refactoring public folder path

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('server/public'))
 app.use(cors({
     origin:'http://localhost:5173',
     methods:['PUT','GET','POST','DELETE'],
