d054dcfe17e0853c981641da986679c8dbcb34b0
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-20 11:51:37 +0300

    Adding endpoint for getting doctors details from the database

--- server/routes/adminActions.route.js
+++ server/routes/adminActions.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { addDoctors } from '../controllers/adminActions.controller.js'
+import { addDoctors, getDoctors } from '../controllers/adminActions.controller.js'
 import { upload } from '../configs/imageUpload.js'
 
 const adminActions = express.Router()
 adminActions.post("/addDoctor",upload.single('image'), addDoctors)
+adminActions.get("/getAllDoctors",getDoctors)
 export default adminActions
