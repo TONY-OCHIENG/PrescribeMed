abdeb7bfad5bfe4d9384faaacd7c5a0853fca4ae
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-22 10:16:49 +0300

    Getting user details from request body

--- server/controllers/patient.controller.js
+++ server/controllers/patient.controller.js
@@ -1,1 +1,11 @@
 
+export const registerPatient = (request,response) => {
+    const {first_name,last_name,email_p,phone_p,image_p,age,password_p} = request.body
+    if (!first_name || !last_name || !email_p
+        || !phone_p || !image_p || !password_p
+    ) {
+        return response.status(200).json({success: false, message: "Please fill all required fields"})
+    }
+
+    
+}
