d92a05db08bfeb81d7190d1fb72e5fe97196dba2
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-22 10:37:25 +0300

    Refactoring request body

--- server/controllers/patient.controller.js
+++ server/controllers/patient.controller.js
@@ -4,7 +4,7 @@ import { hashPassword } from "../configs/hashPassword.js"
 export const registerPatient = (request,response) => {
     const {first_name,last_name,email_p,phone_p,image_p,age,password_p} = request.body
     if (!first_name || !last_name || !email_p
-        || !phone_p || !image_p || !password_p
+        || !phone_p || !image_p || !password_p || !age
     ) {
         return response.status(200).json({success: false, message: "Please fill all required fields"})
     }
