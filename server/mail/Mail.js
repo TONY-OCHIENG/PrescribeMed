6a199d96e76e4bdeb2e80c7634bdf7a3e1c6c70a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-24 11:33:14 +0300

    Creating email transporter

---
+++ server/mail/Mail.js
@@ -0,0 +1,21 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+dotenv.config()
+
+//Function that sends email containing verification code
+
+export const verificationCodeEmail = async (email,verificationCode) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.APP_PASSWORD
+            }
+        })
+    } catch (error) {
+        
+    }
+}
