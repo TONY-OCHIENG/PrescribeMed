ff6ac6a35a93ed52a868c49bac8d2732c63c1cd4
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-25 15:45:34 +0300

    Adding endpoint for email verification

--- server/routes/patient.route.js
+++ server/routes/patient.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { upload } from '../configs/imageUpload.js'
-import { registerPatient } from '../controllers/patient.controller.js'
+import { codeVerification, registerPatient } from '../controllers/patient.controller.js'
 
 const patientRoute = express.Router()
 patientRoute.post("/registerPatient",upload.single('image_p'),registerPatient)
+patientRoute.post("/verifyEmail",codeVerification)
 export default patientRoute
