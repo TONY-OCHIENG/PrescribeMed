4ed40c348d3a1b43ea2b3de9009d884e0e330b45
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-26 11:48:52 +0300

    Adding reset link endpoint

--- server/routes/patient.route.js
+++ server/routes/patient.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { upload } from '../configs/imageUpload.js'
-import { codeVerification, registerPatient } from '../controllers/patient.controller.js'
+import { codeVerification, registerPatient, resetPasswordLink } from '../controllers/patient.controller.js'
 
 const patientRoute = express.Router()
 patientRoute.post("/registerPatient",upload.single('image_p'),registerPatient)
 patientRoute.post("/verifyEmail",codeVerification)
+patientRoute.post("/reset-password",resetPasswordLink)
 export default patientRoute
