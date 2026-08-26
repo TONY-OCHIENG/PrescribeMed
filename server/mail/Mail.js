d827671ec75b1ce2c10b72276dde962e8dff24b6
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-26 11:31:51 +0300

    Creating a transport to send email

--- server/mail/Mail.js
+++ server/mail/Mail.js
@@ -62,5 +62,18 @@ export const welcomeEmail = async (email, fullname) => {
 // A function that sends a reset-password email
 
 export const resetPassword = async (email,url) => {
-    
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: true,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.APP_PASSWORD
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        throw new Error(error)
+    }    
 }
