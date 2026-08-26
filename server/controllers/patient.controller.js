881fd49c63391746495e79c9de1f1f184ad99753
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-26 17:26:45 +0300

    Error handling implementation

--- server/controllers/patient.controller.js
+++ server/controllers/patient.controller.js
@@ -138,5 +138,10 @@ export const resetPasswordLink = (request,response) => {
 
 export const changePassword = (request,response) => {
     const { id } = request.params
-    
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({success: false, message: "Internal server error"})
+    }
 }
