c26685b92210e188920e8e684d8575505fac9712
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-27 10:54:49 +0300

    Implementing a query that updates user's password

--- server/controllers/patient.controller.js
+++ server/controllers/patient.controller.js
@@ -160,7 +160,10 @@ export const changePassword = (request,response) => {
             if (error) return response.status(500).json({success: false, message: error})
             if (result.length > 0) {
                 if (result[0].resetPasswordToken && result[0].resetPasswordTokenExpiresAT >=  new Date().getHours()) {
-                    
+                    const updatePassword = "UPDATE patients SET password = ? WHERE resetPasswordToken = ?"
+                    databaseConnection.query(updatePassword, [hpassword,id],(error,results) => {
+                        if (error) return response.status(500).json({success: false, message: error})
+                    })
                 } else {
                     return response.status(200).json({success: false, message: "Token expired or invalid"})
                 }
