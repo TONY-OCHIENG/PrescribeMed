a212bbe9169f042798d9f37ad3890ce08889e702
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-27 11:04:18 +0300

    Updating password route implementation

--- server/routes/patient.route.js
+++ server/routes/patient.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { upload } from '../configs/imageUpload.js'
-import { codeVerification, registerPatient, resetPasswordLink } from '../controllers/patient.controller.js'
+import { changePassword, codeVerification, registerPatient, resetPasswordLink } from '../controllers/patient.controller.js'
 
 const patientRoute = express.Router()
 patientRoute.post("/registerPatient",upload.single('image_p'),registerPatient)
 patientRoute.post("/verifyEmail",codeVerification)
 patientRoute.post("/reset-link",resetPasswordLink)
+patientRoute.post("/updatePassword/:id",changePassword)
 export default patientRoute
