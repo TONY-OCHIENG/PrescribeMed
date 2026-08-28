969ae3cb2b40f52497a57669dd712e5436259c96
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-28 10:57:34 +0300

    Adding patient authentication middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -17,3 +17,20 @@ export const auth = (request,response,next) => {
         })
     }
 }
+
+export const authPatient = (request,response,next) => {
+    const token = request.cookies.token
+    if (!token) {
+        return response.status(200).json({success: false, message: "Access denied"})
+    } else {
+        jwt.verify(token,process.env.SECRET_USER,(err,decoded) => {
+            if (err) {
+                return response.status(200).json({success: false, message: "Access denied"})
+            } else {
+                request.lastName = decoded.lastName
+                request.patientID = decoded.patientID
+                next()
+            }
+        })
+    }
+}
