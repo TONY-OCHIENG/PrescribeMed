cb576c92008411c34475f0de909258249abfb627
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-28 09:09:44 +0300

    Implementing a function that submits login details to the backend

--- client/src/auth/PatientLogin.jsx
+++ client/src/auth/PatientLogin.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { useState } from 'react'
 import Input from '../components/Input'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
 
 function PatientLogin() {
     const [loading,setLoading] = useState(false)
@@ -18,23 +20,47 @@ function PatientLogin() {
             [name]: value
         }))
     }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setLoading(true)
+        axios.post('http://localhost:5000/api/patients/loginPatient',value)
+        .then((response) => {
+            if (response.data.success) {
+                setLoading(false)
+                toast.success(response.data.message)
+            } else {
+                setLoading(false)
+                toast.error(response.data.message)
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            setLoading(false)
+            toast.error("An error occured")
+        })
+    }
+
   return (
         <div className='flex justify-center items-center h-screen px-2'>
-        <form  className='max-w-md w-full rounded-xl shadow-xl p-2 bg-white/50 backdrop-blur-2xl'>
+        <form  onSubmit={handleSubmit} className='max-w-md w-full rounded-xl shadow-xl p-2 bg-white/50 backdrop-blur-2xl'>
         <h1 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-blue-400 to-blue-600
             text-transparent bg-clip-text'>Welcome Back!</h1>
               <Input
                 icon={Mail}
                 type="email"
                 name="email_p"
+                onChange={handleValues}
                 placeholder="Email"
+                required
             />
              <Input
                 icon={Lock}
                 type="password"
                 name="password"
+                onChange={handleValues}
                 placeholder="Password"
-        
+                required        
             />
             <div className='flex justify-between items-center mb-2'>
                 <div className='flex items-center gap-2'> 
