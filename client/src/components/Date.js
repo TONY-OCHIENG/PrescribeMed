2a0d565049ff09e3601291898660fe7362d21f5a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-31 14:33:27 +0300

    Implementing a function that formats date and time

---
+++ client/src/components/Date.js
@@ -0,0 +1,17 @@
+
+export const formatDate = (date) => {
+    const newDate = new Date(date)
+    const formatted = newDate.toLocaleString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+        });
+
+    return formatted
+
+}
--- client/src/pages/patientPages/PAppointment.jsx
+++ client/src/pages/patientPages/PAppointment.jsx
@@ -1,8 +1,20 @@
 import { FolderOpen } from 'lucide-react'
 import React from 'react'
+import { formatDate } from '../../components/Date';
 
 function PAppointment() {
     let appointment = JSON.parse(localStorage.getItem("appointment")) || []
+    const now = new Date()
+    const formatted = now.toLocaleString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+        });
   return (
     <div className='mt-20 max-w-7xl md:w-[90%] mx-auto w-full px-2 flex flex-col gap-2'>
         <div className='w-full bg-white p-4 shadow-md rounded-md h-[300px]'>
@@ -11,7 +23,19 @@ function PAppointment() {
             </div>
             <div className='w-full shadow-md rounded-md p-4 overflow-auto h-[250px]'>
                 {
-                    appointment?.length > 0 ? "" : <div className='w-full h-full flex justify-center items-center'>
+                    appointment?.length > 0 ? appointment.map((items) => (<div className='flex justify-between md:flex-row flex-col mb-4'>
+                        <div className='flex'>
+                            <div><img src={`http://localhost:5000/images/`+ items.image}  alt="" className='h-[100px] w-[100px] object-cover' /></div>
+                            <div className='ml-3'>
+                                <h1 className='text-md text-gray-600 mb-2 font-extrabold'>Dr, {items.firstName} {items.lastName}</h1>
+                                <h1 className='text-xs text-gray-600'>{items.speciality}</h1>
+                                <h1 className='text-xs text-gray-600'>Phone: {items.phone}</h1>
+                                <h1 className='text-xs text-gray-600'>Time: {formatDate(items.time)}</h1>
+                            </div>
+                        </div>
+                        <div>hello</div>
+                        
+                    </div>)) : <div className='w-full h-full flex justify-center items-center'>
                         <div className='flex flex-col gap-2 items-center'>
                             <FolderOpen className='h-20 w-20 text-blue-400'/>
                             <h1 className='text-gray-600 font-medium'>No appointments made</h1>
