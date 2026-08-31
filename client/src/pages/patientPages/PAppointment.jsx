ce66bf4aa30e607899b602f00f1f78e3f9d05df3
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-08-31 12:20:43 +0300

    Getting items from local storage

--- client/src/pages/patientPages/PAppointment.jsx
+++ client/src/pages/patientPages/PAppointment.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function PAppointment() {
+    let appointment = JSON.parse(localStorage.getItem("appointment")) || []
+    
   return (
     <div className='mt-20 max-w-7xl md:w-[90%] mx-auto w-full px-2 flex flex-col gap-2'>
-        <div className='w-full bg-white p-4 shadow-md rounded-md h-[300px]'></div>
+        <div className='w-full bg-white p-4 shadow-md rounded-md h-[300px]'>
+            <div>
+                <h1 className='text-gray-600 font-extrabold'>Book Appointment</h1>
+            </div>
+            <div className='w-full shadow-md rounded-md p-4 overflow-auto h-[250px]'></div>
+        </div>
         <div className='w-full bg-white p-4 shadow-md rounded-md h-[350px]'></div>
     </div>
   )
