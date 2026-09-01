0e1e2e4674ecf248b3fcb91775b3c073600bd7f0
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-01 11:25:31 +0300

    Booking appointment and inserting appointment details into the database

--- server/controllers/appointment.controller.js
+++ server/controllers/appointment.controller.js
@@ -1,8 +1,13 @@
+import databaseConnection from "../configs/db"
 
 export const bookAppointment = (request,response) => {
     const { doctors_id, patient_id, date, appointmentFee } = request.body
     try {
-        
+        const insertAppointment = "INSERT INTO appointments(doctors_id,patient_id,appointmentDate,appointmentFee) VALUES(?,?,?,?)"
+        databaseConnection.query(insertAppointment,[doctors_id,patient_id,date,appointmentFee], (error, results) => {
+            if (error) return response.status(500).json({success: false, message: error})
+            return response.status(200).json({success: true, message: "Appointment booked successfully"})
+        })
     } catch (error) {
         console.log(error)
         return response.status(500).json({success: false, message: "Internal server error"})
