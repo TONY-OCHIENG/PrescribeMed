723a438c721ebf2fc82b307d71465775f78c4bf0
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-02 10:23:58 +0300

    Error handling

--- server/controllers/appointment.controller.js
+++ server/controllers/appointment.controller.js
@@ -13,3 +13,12 @@ export const bookAppointment = (request,response) => {
         return response.status(500).json({success: false, message: "Internal server error"})
     }
 }
+
+export const appointmentHistory = (request,response) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({success: false, message: "Internal server error"})
+    }
+}
