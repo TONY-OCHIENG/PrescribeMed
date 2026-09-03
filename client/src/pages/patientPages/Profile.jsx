5d751e11600d343f22f0b4c99f0d166f0c6d3529
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-03 09:51:13 +0300

    Adding form element

--- client/src/pages/patientPages/Profile.jsx
+++ client/src/pages/patientPages/Profile.jsx
@@ -35,7 +35,24 @@ function Profile() {
   return (
     <div className='mt-20 w-full'>
         <div className='max-w-7xl md:w-[90%] mx-auto bg-white p-4 shadow-md rounded-xl flex justify-center items-center'>
-
+          <form action="" className='p-4 rounded-md shadow-md max-w-md'>
+            <div className='flex justify-center items-center'>
+                <img src={`http://localhost:5000/images/`+ profile.map((item) => item.image_p)[0]}   alt="" className='h-[100px] w-[100px] mb-2 rounded-full'/>
+            </div>
+            <label htmlFor="firstname">First name</label>
+            <input type="text" id='firstname' className='p-2 rounded-md border w-full'/>
+            <label htmlFor="lastname">Last name</label>
+            <input type="text" id='lastname' className='p-2 rounded-md border w-full'/>
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' className='p-2 rounded-md border w-full'/>
+            <label htmlFor="phone">Phone</label>
+            <input type="text" id='phone' className='p-2 rounded-md border w-full'/>
+            <label htmlFor="age">Age</label>
+            <input type="number" id='age' className='p-2 rounded-md border w-full'/>
+            <label htmlFor="image">Image</label>
+            <input type="file" id='image' className='p-2 rounded-md border w-full'/>
+            <button className='w-full py-2 bg-blue-500 font-extrabold text-white mt-4 rounded-md cursor-pointer'>Edit Profile</button>
+          </form>
         </div>
     </div>
   )
