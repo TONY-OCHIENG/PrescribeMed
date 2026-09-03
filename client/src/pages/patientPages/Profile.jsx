000de82332b745cf4891ebb87c1411459ba8729f
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-03 10:45:02 +0300

    Displaying user's image

--- client/src/pages/patientPages/Profile.jsx
+++ client/src/pages/patientPages/Profile.jsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react'
 function Profile() {
     const [patientId, setPatientID] = useState([])
     const [profile, setProfile] = useState([])
+    const [image, setImage] = useState([])
  
     useEffect(() => {
         axios.get('http://localhost:5000/api/patients/authPatient')
@@ -26,6 +27,7 @@ function Profile() {
         .then((response) => {
             if (response.data.success) {
                 setProfile(response.data.results[0])
+                setImage(response.data.results[0].image_p)
             } 
         })
         .catch((error) => {
@@ -38,7 +40,7 @@ function Profile() {
         <div className='max-w-7xl md:w-[90%] mx-auto bg-white p-4 shadow-md rounded-xl flex justify-center items-center'>
           <form action="" className='p-4 rounded-md shadow-md max-w-md'>
             <div className='flex justify-center items-center'>
-                <img src={`http://localhost:5000/images/`+ profile.image_p}   alt="" className='h-[100px] w-[100px] mb-2 rounded-full'/>
+                <img src={`http://localhost:5000/images/`+ image}   alt="" className='h-[100px] w-[100px] mb-2 rounded-full'/>
             </div>
             <label htmlFor="firstname">First name</label>
             <input type="text" name='first_name' value={profile.first_name} onChange={(event) => setProfile({...profile, first_name: event.target.value})} id='firstname' className='p-2 rounded-md border w-full'/>
@@ -51,7 +53,7 @@ function Profile() {
             <label htmlFor="age">Age</label>
             <input type="number" name='age' value={profile.age}  onChange={(event) => setProfile({...profile, age: event.target.value})} id='age' className='p-2 rounded-md border w-full'/>
             <label htmlFor="image">Image</label>
-            <input type="file" name='image_p' id='image' className='p-2 rounded-md border w-full'/>
+            <input type="file" name='image_p' onChange={(event) => setProfile({...profile, image_p: event.target.value})} id='image' className='p-2 rounded-md border w-full'/>
             <button className='w-full py-2 bg-blue-500 font-extrabold text-white mt-4 rounded-md cursor-pointer'>Edit Profile</button>
           </form>
         </div>
