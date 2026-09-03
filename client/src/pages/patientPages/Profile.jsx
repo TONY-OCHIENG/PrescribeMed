33464ff0c1625bbd00091ee26a215b0adeefaf1b
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-03 09:08:25 +0300

    Fetchng patient details from the database

--- client/src/pages/patientPages/Profile.jsx
+++ client/src/pages/patientPages/Profile.jsx
@@ -5,6 +5,7 @@ import { useEffect } from 'react'
 
 function Profile() {
     const [patientId, setPatientID] = useState([])
+    const [profile, setProfile] = useState([])
     useEffect(() => {
         axios.get('http://localhost:5000/api/patients/authPatient')
         .then((response) => {
@@ -18,7 +19,19 @@ function Profile() {
             console.log(error)
         })
     },[])
-    console.log(patientId)
+    // fetching patient details from the database
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/patients/patientProfile/${patientId.patientID}`)
+        .then((response) => {
+            if (response.data.success) {
+                setProfile(response.data.results)
+            } 
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    },[patientId])
+  
   return (
     <div className='mt-20 w-full'>
         <div className='max-w-7xl md:w-[90%] mx-auto bg-white p-4 shadow-md rounded-xl flex justify-center items-center'>
