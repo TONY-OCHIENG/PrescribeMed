3356988637fc809e33e5da579e88ac08cfbef5b4
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-05 12:52:28 +0300

    Error handling

--- server/controllers/doctor.controller.js
+++ server/controllers/doctor.controller.js
@@ -1,4 +1,15 @@
 
 export const loginDoctor = (request,response) => {
     const { email, password} = request.body
+
+    if (!email || !password) {
+        return response.status(200).json({success: false, message: "Fill all fields"})
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({success: false, message: "Internal server error"})
+    }
 } 
