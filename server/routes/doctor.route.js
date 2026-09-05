63ced2b81f949f25c80ac431a85e03993d687575
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-05 15:41:47 +0300

    Logout doctor endpoint implementation

--- server/routes/doctor.route.js
+++ server/routes/doctor.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { authDoctorLogin, loginDoctor } from '../controllers/doctor.controller.js'
+import { authDoctorLogin, loginDoctor, logoutDoctor } from '../controllers/doctor.controller.js'
 import { authDoctor } from '../middlewares/auth.js'
 
 const doctorRoutes = express.Router()
 doctorRoutes.post("/loginDoctor",loginDoctor)
 doctorRoutes.get("/authDoctor",authDoctor,authDoctorLogin)
+doctorRoutes.get("/logoutDoctor",logoutDoctor)
 export default doctorRoutes
