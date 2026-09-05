16e8135314734c304ee634ef461287e89170d97a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-05 15:36:51 +0300

    Authenticating doctor

--- client/src/pages/DoctorPages/DDashboard.jsx
+++ client/src/pages/DoctorPages/DDashboard.jsx
@@ -1,14 +1,35 @@
+import axios from 'axios'
 import { BriefcaseMedical, LayoutDashboard, NotebookPen, Power, SquarePlus } from 'lucide-react'
-import React from 'react'
-import { NavLink, Outlet } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 
 function DDashboard() {
+    const [lastName, setLastName] = useState([])
+    const navigate = useNavigate()
       const navLinks = [
         {name:"Dashboard",link:"/doctor",icon:LayoutDashboard},
         {name:"Appointments",link:"/admin/appointments",icon:NotebookPen},
         {name:"Doctors",link:"/admin/doctors",icon:BriefcaseMedical},
         {name:"Add doctors",link:"/admin/add-doctors",icon:SquarePlus}
     ]
+
+    axios.defaults.withCredentials = true
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/doctors/authDoctor')
+        .then((response) => {
+            if (response.data.success) {
+                 setLastName(response.data.details)
+            } else {
+                navigate("/doctor-login")
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            navigate('/doctor-login')
+        })
+    },[])
+    
   return (
      <div className='flex w-[100%] h-screen'>
         <div className='bg-white inset-y-0 left-0 w-[20%] md:w-[15%] border border-l'>
@@ -30,12 +51,13 @@ function DDashboard() {
         </div>
         <div className='w-[80%] md:w-[85%] overflow-x-auto'>
             <div className='fixed w-full z-50 h-[8%] shadow-md p-4 flex items-center bg-white border-b'>
-                <div className='w-[80%]  flex justify-end '>
-                    <button  className='py-2 px-6 cursor-pointer bg-blue-500 flex justify-between items-center rounded-full'>
-                     <Power className='mr-2 text-white'/>
-                     <p className='text-white font-extrabold'>Logout</p>
-                    </button>
-                </div>
+                    <div className='w-[80%]  flex justify-end items-center'>
+                        <h1 className='text-xl text-gray-600 mr-5 font-extrabold'>Dr, {lastName.lastName}</h1>
+                        <button onClick={() => handleLogout()} className='py-2 px-6 cursor-pointer bg-blue-500 flex justify-between items-center rounded-full'>
+                        <Power className='mr-2 text-white'/>
+                        <p className='text-white font-extrabold'>Logout</p>
+                        </button>
+                    </div>
             </div>
             <Outlet/>
         </div>
