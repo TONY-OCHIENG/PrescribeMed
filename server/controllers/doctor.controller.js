9d95c3f7f28352af360028d983b0465eb8ec888d
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-05 13:08:57 +0300

    Selecting doctor by email from the database

--- server/controllers/doctor.controller.js
+++ server/controllers/doctor.controller.js
@@ -1,3 +1,4 @@
+import databaseConnection from "../configs/db.js"
 
 export const loginDoctor = (request,response) => {
     const { email, password} = request.body
@@ -7,7 +8,10 @@ export const loginDoctor = (request,response) => {
     }
 
     try {
-        
+        const checkDoctor = "SELECT doctors_id,firstName,lastName,password FROM doctors WHERE email = ?"
+        databaseConnection.query(checkDoctor,[email],(error,result) => {
+            if (error) return response.status(500).json({success: false, message: error})
+        })        
     } catch (error) {
         console.log(error)
         return response.status(500).json({success: false, message: "Internal server error"})
