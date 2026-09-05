27453e2e8d58e74dadac21221a8bc23811b66a99
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-05 13:27:52 +0300

    Implementing a function that handles submit login details

--- client/src/auth/DoctorLogin.jsx
+++ client/src/auth/DoctorLogin.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Input from '../components/Input'
 import { Loader, Lock, Mail } from 'lucide-react'
 import axios from 'axios'
+import toast from 'react-hot-toast'
 
 function DoctorLogin() {
     const [doctor,setDoctor] = useState({
@@ -18,9 +19,27 @@ function DoctorLogin() {
             [name] : value
         }))
     }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setLoading(true)
+        axios.post('http://localhost:5000/api/doctors/loginDoctor',doctor)
+        .then((response) => {
+            if (response.status.success) {
+                setLoading(false)
+                toast.success(response.data.message)
+            } else {
+                setLoading(false)
+                toast.error(response.data.message)
+            }
+        })
+        .catch((error) => {
+            toast.error("An error occured")
+            setLoading(false)
+        })
+    }
   return (
       <div className='flex justify-center items-center h-screen px-2'>
-        <form  className='max-w-md w-full rounded-xl shadow-xl p-2 bg-white/50 backdrop-blur-2xl'>
+        <form onSubmit={handleSubmit} className='max-w-md w-full rounded-xl shadow-xl p-2 bg-white/50 backdrop-blur-2xl'>
         <h1 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-blue-400 to-blue-600
             text-transparent bg-clip-text'>Login Doctor</h1>
               <Input
@@ -28,12 +47,14 @@ function DoctorLogin() {
                 type="email"
                 name="email"
                 placeholder="Email"
+                onChange = {handleChange}
             />
              <Input
                 icon={Lock}
                 type="password"
                 name="password"
-                placeholder="Password"               
+                placeholder="Password"  
+                onChange = {handleChange}             
             />
             <button disabled={loading} className='w-full text-white bg-gradient-to-r from-blue-400 to-blue-600 py-2
             font-extrabold cursor-pointer rounded-md text-xl flex justify-center items-center
