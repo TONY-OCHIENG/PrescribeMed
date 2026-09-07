bab4e6dfba100abdcd6d3e70437c60a73706914b
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-07 16:26:02 +0300

    Displaying appointments

--- client/src/pages/DoctorPages/Doctor.jsx
+++ client/src/pages/DoctorPages/Doctor.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
-import { HospitalIcon, NotebookPenIcon, Wallet } from 'lucide-react'
+import { FolderOpen, HospitalIcon, NotebookPenIcon, Wallet } from 'lucide-react'
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import { formatDate } from '../../components/Date'
 
 function Doctor() {
     const [doctor_id,setDoctorID] = useState([])
@@ -10,6 +11,7 @@ function Doctor() {
     useEffect(() => {
             axios.get('http://localhost:5000/api/doctors/authDoctor')
             .then((response) => {
+                console.log(response)
                 if (response.data.success) {
                      setDoctorID(response.data.details)
                 } else {
@@ -25,6 +27,7 @@ function Doctor() {
     useEffect(() => {
         axios.get(`http://localhost:5000/api/appointment/appointmentDoctor/${doctor_id.doctorID}`)
         .then((response) => {
+            console.log(response)
             if (response.data.success) {
                 setAppointment(response.data.results)
             }
@@ -32,7 +35,7 @@ function Doctor() {
         .catch((error) => {
             console.log(error)
         })
-    },[doctor_id.doctorID])
+    },[doctor_id])
     
   return (
     <div className='mt-20 max-w-7xl md:w-[90%] mx-auto px-2'>
@@ -80,8 +83,46 @@ function Doctor() {
                 </div>
              </div>       
         </div>
-        <h1 className='text-gray-600 mt-4 font-extrabold'>Recent Appointments</h1>
-        <div className='bg-white p-4 rounded-md mt-3'></div>
+      <div className='w-full bg-white p-4 shadow-md rounded-md h-[400px] mt-10'>
+            <h1 className='text-gray-600 font-extrabold'>Recent Appointments</h1>
+          <div className='mt-2 overflow-auto bg-white shadow-md rounded-md h-[330px] p-2'>
+           {
+            appointment.length > 0 ?
+                <table className='w-full text-left text-gray-600'>
+                    <thead>
+                        <th>Image</th>
+                        <th>Patient</th>
+                        <th>Image</th>
+                        <th>Doctor</th>
+                        <th>Date</th>
+                        <th>Fee</th>
+                        <th>Status</th>
+                    </thead>
+                    <tbody className='p-2 '>
+                        {
+                            appointment.map((item) => (
+                                 <tr className='text-xs even:bg-gray-100'>
+                                    <td className='p-2'>
+                                    <img src={`http://localhost:5000/images/`+ item.patient_image}  alt="" className='h-[50px] w-[50px] rounded-full' />
+                                    </td>   
+                                    <td>{item.patient_first_name} {item.patient_last_name}</td>
+                                    <td><img src={`http://localhost:5000/images/`+ item.doctor_image}  alt="" className='h-[50px] w-[50px] rounded-full' /></td>
+                                    <td>{item.doctor_first_name} {item.doctor_last_name}</td>
+                                    <td className='w-[200px]'>{formatDate(item.appointmentDate)}</td> 
+                                    <td>{item.appointmentFee}</td>
+                                    <td>{item.appointmentStatus}</td>                    
+                                </tr>               
+                            ))
+                        }
+                      
+                    </tbody>
+                </table>
+               : <div className='w-full h-full flex justify-center items-center'>
+               <FolderOpen className='h-20 w-20 text-blue-400'/>
+            </div>
+           }
+          </div>
+        </div>
     </div>
   )
 }
