45068810548548f2ba60927322e8258b35de702a
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-08 08:29:12 +0300

    Getting patient phone number

--- server/controllers/appointment.controller.js
+++ server/controllers/appointment.controller.js
@@ -36,7 +36,7 @@ export const appointmentHistory = (request,response) => {
 export const appointmentHistroryDoctor = (request,response) => {
     const { id } = request.params
     try {
-        const appointmentDoctor = "SELECT d.firstName AS doctor_first_name, d.lastName AS doctor_last_name, d.image AS doctor_image, p.first_name AS patient_first_name, p.last_name AS patient_last_name, p.image_p AS patient_image, a.appointmentDate, a.appointmentStatus, a.appointmentFee FROM appointments a INNER JOIN doctors d ON d.doctors_id = a.doctors_id INNER JOIN patients p ON p.patient_id = a.patient_id WHERE a.appointmentStatus = 'pending' AND d.doctors_id = ? ORDER BY a.appointmentDate DESC"
+        const appointmentDoctor = "SELECT d.firstName AS doctor_first_name, d.lastName AS doctor_last_name, d.image AS doctor_image, p.first_name AS patient_first_name, p.last_name AS patient_last_name, p.phone_p AS patient_phone, p.image_p AS patient_image, a.appointmentDate, a.appointmentStatus, a.appointmentFee FROM appointments a INNER JOIN doctors d ON d.doctors_id = a.doctors_id INNER JOIN patients p ON p.patient_id = a.patient_id WHERE a.appointmentStatus = 'pending' AND d.doctors_id = ? ORDER BY a.appointmentDate DESC"
         databaseConnection.query(appointmentDoctor,[id],(error,results) => {
             if (error) return response.status(500).json({success: false, message: error})
             if (results.length > 0) {
