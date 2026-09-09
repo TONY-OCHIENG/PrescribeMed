20ca9703177a5797390d9418eb97f3a025be25b3
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-09 08:19:41 +0300

    Implementing an endpoint that gets doctor's profile

--- server/routes/doctor.route.js
+++ server/routes/doctor.route.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { authDoctorLogin, loginDoctor, logoutDoctor } from '../controllers/doctor.controller.js'
+import { authDoctorLogin, doctorsProfile, loginDoctor, logoutDoctor } from '../controllers/doctor.controller.js'
 import { authDoctor } from '../middlewares/auth.js'
 
 const doctorRoutes = express.Router()
 doctorRoutes.post("/loginDoctor",loginDoctor)
 doctorRoutes.get("/authDoctor",authDoctor,authDoctorLogin)
 doctorRoutes.get("/logoutDoctor",logoutDoctor)
+doctorRoutes.get("/doctorProfile/:id",doctorsProfile)
 export default doctorRoutes
