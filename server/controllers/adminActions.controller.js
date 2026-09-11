f04902dbcadae982128f83e9c1afdbfd4d6d8e85
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-11 09:10:32 +0300

    Implementing a controller that counts the total number of appointments

--- server/controllers/adminActions.controller.js
+++ server/controllers/adminActions.controller.js
@@ -130,3 +130,20 @@ export const numberofPatients = (request,response) => {
         return response.status(500).json({success: false, message: "Internal server error"})
     }
 }
+
+export const numberofAppointments = (request,response) => {
+    try {
+        const numberofDoctors = "SELECT COUNT(appointment_id) AS totalAppointments FROM appointments"
+        databaseConnection.query(numberofDoctors,(error,results) => {
+            if (error) return response.status(500).json({success: false, message: error})
+            if (results.length > 0) {
+                return response.status(200).json({success: true, results: results[0].totalAppointments})
+            } else {
+                return response.status(200).json({success: false})
+            }
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({success: false, message: "Internal server error"})
+    }
+}
