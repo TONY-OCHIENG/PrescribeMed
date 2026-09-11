5ba1ab65387ac42564c284302316c53f309bc2ee
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-11 10:19:33 +0300

    Displaying all appointments

--- client/src/pages/Appointments.jsx
+++ client/src/pages/Appointments.jsx
@@ -1,24 +1,60 @@
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { formatDate } from '../components/Date'
 
 function Appointments() {
+  const [appointment,setAppointment] = useState([])
+   useEffect(() => {
+          axios.get("http://localhost:5000/api/appointment/appointmentHistory")
+          .then((response) => {
+              if (response.data.success) {
+                  setAppointment(response.data.results)
+              }
+          })
+          .catch((error) => {
+              console.log(error)
+          })
+    },[])
   return (
     <div className='w-full md:max-w-[90%] px-2 mx-auto '>
       <h1 className='mt-25 text-gray-600 font-extrabold'>Appointment history</h1>
-       <div className='p-4 rounded-md shadow-md mt-4 bg-white h-[550px] overflow-y-auto overflow-x-auto'>
-        <table className='w-full'>
-            <thead className='text-xs text-left'>
-                <th>Doctor's Image</th>
-                <th>Doctor's Name</th>
-                <th>Patient's Image</th>
-                <th>Patient's Name</th>
-                <th>Appointment Date</th>
-                <th>Appointment Fee</th>
-                <th>Status</th>
-                <th>Action</th>
-            </thead>
-        </table>
-      </div> 
-    </div>
+        <div className='p-4 rounded-md shadow-md mt-4 bg-white h-[550px] overflow-y-auto overflow-x-auto'>
+            <table className='w-full text-left text-gray-600'>
+                <thead>
+                    <th>Image</th>
+                    <th>Patient</th>
+                    <th>Phone</th>
+                    <th>Image</th>
+                    <th>Doctor</th>
+                    <th>Date</th>
+                    <th>Fee</th>
+                    <th>Status</th>
+                </thead>
+                  <tbody className='p-2 '>
+                    {
+                        appointment.map((item) => (
+                              <tr className='text-xs even:bg-gray-100 even:bg-gray-100'>
+                                <td className='p-2'>
+                                <img src={`http://localhost:5000/images/`+ item.patient_image}  alt="" className='h-[50px] w-[50px] rounded-full' />
+                                </td>   
+                                <td>{item.patient_first_name} {item.patient_last_name}</td>
+                                <td>{item.patient_phone}</td>
+                                <td><img src={`http://localhost:5000/images/`+ item.doctor_image}  alt="" className='h-[50px] w-[50px] rounded-full' /></td>
+                                <td>{item.doctor_first_name} {item.doctor_last_name}</td>
+                                <td className='w-[200px]'>{formatDate(item.appointmentDate)}</td> 
+                                <td>{item.appointmentFee}</td>
+                                <td>{item.appointmentStatus}</td>                
+                            </tr>               
+                        ))
+                    }
+                  
+                </tbody>
+            </table>
+        </div> 
+      </div>
+
   )
 }
 
