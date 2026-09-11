0a3f4c99d62e93b0f2d2dd816ef3468328eb4647
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-11 08:34:35 +0300

    Error handling

--- server/controllers/adminActions.controller.js
+++ server/controllers/adminActions.controller.js
@@ -96,3 +96,13 @@ export const deleteDoctor = (request,response) => {
         return response.status(500).json({success: false, message: error})
     }
 }
+
+export const numberofDoctors = (request,response) => {
+    const { id } = request.params
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        return response.status(500).json({success: false, message: "Internal server error"})
+    }
+}
