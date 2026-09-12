fb434ad294d82f70c27050bfee1f0019e4af8ec8
TONY-OCHIENG <tonyochiengdh@gmail.com>
2026-09-12 14:19:47 +0300

    Adding about sections

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import Navigation from '../components/Navigation'
+import About from './About'
 
 function Home() {
   return (
     <div>
      <Navigation/>   
-     <Banner/>         
+     <Banner/>  
+     <About/>       
     </div>
   )
 }
